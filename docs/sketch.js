9ec6724d2749b6e4c0458cd32ec2dfc93b1baff9
saquantum <517393456@qq.com>
2025-02-04 20:40:04 +0000

    Update sketch.js

--- docs/sketch.js
+++ docs/sketch.js
@@ -7,7 +7,7 @@ function preload() {
   img = loadImage("back.webp");
 }
 
-let SxSlider, SySlider, spanSlider, rotSlider;
+let SxSlider, SySlider, spanSlider, rotSlider, HySlider;
 
 function setup() {
   createCanvas(canvasX, canvasY, WEBGL);
@@ -16,33 +16,48 @@ function setup() {
   createSliderWithLabel("Sy: ", 0.1, 1.0, 0.5 / sqrt(2), 0.01, (slider) => (SySlider = slider));
   createSliderWithLabel("Span: ", 0, PI, PI / 2, 0.01, (slider) => (spanSlider = slider));
   createSliderWithLabel("Rot: ", 0, 2*PI, PI / 4, 0.01, (slider) => (rotSlider = slider));
-  
+  createSliderWithLabel("Hy: ", 0.01, 2.0, 1.0, 0.01, (slider) => (HySlider = slider));
 }
 
-function draw(){
+function draw() {
   background(200);
-  
+  stroke(0); // Black border
+  strokeWeight(2); // Line thickness
+
   let Sx = SxSlider.value();
   let Sy = SySlider.value();
   let span = spanSlider.value();
   let rot = rotSlider.value();
-  
-  
-  beginShape();
-  texture(img);
-  
-  // T = { {Sx*cos(rotate), Sy*cos(span+rotate)},  {Sx*sin(rotate), Sy*sin(span+rotate)}}
-  // x' = x*Sx*cos(rotate) + y*Sy*cos(span+rotate);
-  // y' = x*Sx*sin(rotate) + y*Sy*sin(span+rotate);
-  // Sx Sy: scaling factor
-  // span: the angle between new x and y axis
-  // rot: the rotation angle to new coordinate
-  
-  vertex(newCoorX(-canvasX/2,-canvasY/2,Sx,Sy,span,rot),newCoorY(-canvasX/2,-canvasY/2,Sx,Sy,span,rot), 0, 0, 0); // Top-left corner
-  vertex(newCoorX(canvasX/2,-canvasY/2,Sx,Sy,span,rot),newCoorY(canvasX/2,-canvasY/2,Sx,Sy,span,rot), 0, 0, img.width, 0); // Top-right corner
-  vertex(newCoorX(canvasX/2,canvasY/2,Sx,Sy,span,rot),newCoorY(canvasX/2,canvasY/2,Sx,Sy,span,rot), img.width, img.height); // Bottom-right corner
-  vertex(newCoorX(-canvasX/2,canvasY/2,Sx,Sy,span,rot),newCoorY(-canvasX/2,canvasY/2,Sx,Sy,span,rot), 0, 0, img.height);
-  endShape(CLOSE);
+  let Hy = HySlider.value();
+
+  let gridSize = 8;
+  let cellWidth = canvasX / gridSize;
+  let cellHeight = canvasY / gridSize;
+
+  // Draw grid lines
+  for (let i = 0; i <= gridSize; i++) {
+    let x = -canvasX / 2 + i * cellWidth;
+    let y = -canvasY / 2 + i * cellHeight;
+
+    // Apply transformation using newCoorX and newCoorY
+    let x1 = newCoorX(x, -canvasY / 2, Sx, Sy, span, rot);
+    let y1 = Hy * newCoorY(x, -canvasY / 2, Sx, Sy, span, rot);
+
+    let x2 = newCoorX(x, canvasY / 2, Sx, Sy, span, rot);
+    let y2 = Hy * newCoorY(x, canvasY / 2, Sx, Sy, span, rot);
+
+    let x3 = newCoorX(-canvasX / 2, y, Sx, Sy, span, rot);
+    let y3 = Hy * newCoorY(-canvasX / 2, y, Sx, Sy, span, rot);
+
+    let x4 = newCoorX(canvasX / 2, y, Sx, Sy, span, rot);
+    let y4 = Hy * newCoorY(canvasX / 2, y, Sx, Sy, span, rot);
+
+    // Draw vertical lines
+    line(x1, y1, x2, y2);
+
+    // Draw horizontal lines
+    line(x3, y3, x4, y4);
+  }
 }
 
 function newCoorX(x, y, Sx, Sy, span, rot){
