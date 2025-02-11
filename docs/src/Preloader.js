216885588e548669b16fdb8f3e6fcbbbb1fb7710
saquantum <517393456@qq.com>
2025-02-11 22:21:44 +0000

    Update Preloader.js

--- docs/src/Preloader.js
+++ docs/src/Preloader.js
@@ -1,7 +1,7 @@
 export function preloader(p5){
 
     const isGithubPages = window.location.hostname.includes("github.io");
-    const basePath = isGithubPages ? "https://github.com/saquantum/demo/tree/main/docs/assets/images/" : "../assets/images/";
+    const basePath = isGithubPages ? "./assets/images/" : "../assets/images/";
 
     let images = new Map(); // <String name, Image img>
     images.set("Tree", p5.loadImage(basePath + "Tree.jpg"));
